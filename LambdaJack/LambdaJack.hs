0caf660e8a0e645969012c6c3f17af67bb58b9ea
guillen <oguillen@turpialdev.com>
2016-02-17 01:59:26 -0430

    more lambdajack

--- LambdaJack/LambdaJack.hs
+++ LambdaJack/LambdaJack.hs
@@ -12,13 +12,20 @@ module LambdaJack where
 data Card = Card {
 	lvalue :: Value,
 	suit :: Suit
-}
-data Suit = Clubs | Diamonds | Spades | Hearts
-data Value = Numeric Int | Jack | Queen | King | Ace
+} deriving(Show)
+
+
+size :: Hand -> Int
+size (H xs) = length xs
+
+data Suit = Clubs | Diamonds | Spades | Hearts deriving(Show)
+data Value = Numeric Int | Jack | Queen | King | Ace deriving(Show)
 
 newtype Hand = H [Card]
+	deriving(Show)
 
 data Player = LambdaJack | You
+			deriving(Show)
 
 
 value :: Hand -> Int
@@ -32,3 +39,29 @@ value (H cards) = if (evaluate 11 cards) > 21 then evaluate 1 cards
 				sumValue ace i (Card King _) = i + 10
 				sumValue ace i (Card Queen _) = i + 10
 				sumValue ace i (Card Ace _) = i + ace
+
+
+busted :: Hand -> Bool
+busted h = value h > 21
+
+
+winner :: Hand -> Hand -> Player
+winner player lambda
+	| not (busted player) && not (busted lambda) = who (value player) (value lambda)
+	| busted player && not(busted lambda) = LambdaJack
+	| not(busted player) && busted lambda = You
+	| otherwise = LambdaJack
+	where
+		who p l
+			| p > l = You
+			| p < l = LambdaJack
+			| otherwise = LambdaJack
+
+
+fullDeck :: Hand
+fullDeck = H $ merge [Card (Numeric n) y | n <- [2..10], 
+										   y <- [Clubs,Diamonds,Spades,Hearts]] 
+					 [Card x y | x <- [Jack,Queen,King,Ace], 
+						         y <- [Clubs,Diamonds,Spades,Hearts]]
+	where
+		merge a b = a++b
